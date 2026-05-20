aed451197a1a64b863f486180683ea4afa939688
ValdikSS <iam@valdikss.org.ru>
2026-05-20 18:23:18 +0300

    Fix last band in page compression

--- src/rastertocapt.c
+++ src/rastertocapt.c
@@ -173,7 +173,7 @@ static void compress_page_data(struct printer_state_s *state,
 			}
 			memcpy(bandbuf + iline * dims->line_size + shiftb, linebuf + shiftl, csize);
 		}
-		size = state->ops->compress_band(state, compbuf, compsize, bandbuf, dims->line_size, nlines);
+		size = state->ops->compress_band(state, compbuf, compsize, bandbuf, dims->line_size, nlines, nlines < dims->band_size);
 		new_band = calloc(1, sizeof_struct_band_list_s(size));
 		if (! new_band)
 			abort();
